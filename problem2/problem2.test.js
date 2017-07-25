b8991f923151449b513fd80eed56fede84234909
jenniferwiner <jeniwiner@gmail.com>
2017-07-25 10:27:56 -0600

    test for problem 2

--- problem2/problem2.test.js
+++ problem2/problem2.test.js
@@ -1,7 +1,9 @@
 var assert = require('chai').assert
 var expect = require('chai').expect
-var sumMultiples = require('./problem1')
+var evenFib = require('./problem2')
 
 describe('Even Fib', function() {
-
+  it('should return sum of the even Fibonacci numbers below 4,000,000', function() {
+    expect(evenFib()).to.deep.equal(4613732)
+  });
 });
