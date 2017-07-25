ac3297030474d3426b69617ba343651c1f9c729f
jenniferwiner <jeniwiner@gmail.com>
2017-07-25 10:29:06 -0600

    rewrite test describe

--- problem1/problem1.test.js
+++ problem1/problem1.test.js
@@ -2,8 +2,8 @@ var assert = require('chai').assert
 var expect = require('chai').expect
 var sumMultiples = require('./problem1')
 
-describe('Find the sum of the multiples of 3 or 5 below 1000', function() {
-  it('should return ', function() {
+describe('sumMultiples', function() {
+  it('Find the sum of the multiples of 3 or 5 below 1000', function() {
     expect(sumMultiples()).to.deep.equal(233168)
   });
 });
