c37fb42ac36a1371de5bdf23fe23d6ec7de9e818
jenniferwiner <jeniwiner@gmail.com>
2017-08-09 13:58:40 -0600

    setup problem 4 - largest palidrome

---
+++ problem4/problem4.test.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert
+var expect = require('chai').expect
+var largestPalindrome = require('./problem4')
+
+describe('Largest Palindrome', function() {
+  it('should find the largest palindrome made from the product of two 3-digit numbers', function() {
+    expect(largestPalindrome()).to.deep.equal()
+  });
+
+  });
+});
