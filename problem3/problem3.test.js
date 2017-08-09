803c00e37d2027f689c66bafe592ca23992c6eeb
jenniferwiner <jeniwiner@gmail.com>
2017-08-09 13:55:10 -0600

    add more test cases

--- problem3/problem3.test.js
+++ problem3/problem3.test.js
@@ -6,4 +6,7 @@ describe('Largest Prime', function() {
   it('should return the largest prime factor of the number 600851475143', function() {
     expect(largestPrime(600851475143)).to.deep.equal(6857)
   });
+  it('should return the largest prime factor of the number 13195', function() {
+    expect(largestPrime(13195)).to.deep.equal(29)
+  });
 });
