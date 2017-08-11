b538df336d03bf4b906421cc68a63b2e71de553f
jenniferwiner <jeniwiner@gmail.com>
2017-08-11 11:13:48 -0600

    does not solve

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -2,7 +2,22 @@
 
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-function largestPalindrome(n) {
+// this is not finding the largest number, just a palindrome... because of the way it is incrementing
+function largestPalindrome() {
+  for (var i = 999; i > 0; i--) {
+    for (var j = 999; i > 0; i--) {
+      if (isPal(i*j)) {
+        return i*j
+      }
+    }
+  }
 }
 
+function isPal(n){
+  let nString = n.toString()
+  let firstHalf = nString.slice(0, Math.floor(nString.length/2))
+  let secondHalf = nString.length % 2 === 0 ? nString.slice(nString.length/2) : nString.slice(Math.floor(nString.length/2) + 1)
+  return firstHalf === secondHalf.split('').reverse().join('')
+}
+largestPalindrome()
 module.exports = largestPalindrome
